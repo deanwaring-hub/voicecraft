9c01ba7cb240b00af94a7e62ee4362bb3a330761
DeanWaringDev <contact@deanwaring.co.uk>
2026-02-19 12:29:12 +0000

    Added S3 Bucket Uploads

--- js/auth-config.js
+++ js/auth-config.js
@@ -1,24 +1,26 @@
-1/**
- * VoiceCraft - Cognito Auth Configuration
- * Single source of truth - update these values if you ever recreate the pool
+/**
+ * VoiceCraft - Auth & AWS Configuration
+ * Single source of truth - update these values if you ever recreate resources
  */
 const AUTH_CONFIG = {
-    region:        'eu-west-2',
-    userPoolId:    'eu-west-2_9G6wvet1N',
-    clientId:      '79bnu9nead8lfbvf158pr2ppuf',
-    domain:        'https://cognito-idp.eu-west-2.amazonaws.com/eu-west-2_9G6wvet1N',
+    region:           'eu-west-2',
+    userPoolId:       'eu-west-2_9G6wvet1N',
+    clientId:         '79bnu9nead8lfbvf158pr2ppuf',
+    identityPoolId:   'eu-west-2:54a7d7ad-80f4-46af-baed-cbb7550f5646',
 
-    // The Cognito Hosted UI domain (for login redirects)
-    // Found in Cognito > User Pool > App integration > Domain
-    // Looks like: https://voicecraft-web.auth.eu-west-2.amazoncognito.com
-    hostedUiDomain: 'https://eu-west-29g6wvet1n.auth.eu-west-2.amazoncognito.com',
+    // Cognito Hosted UI domain
+    hostedUiDomain:   'https://eu-west-29g6wvet1n.auth.eu-west-2.amazoncognito.com',
 
     // Where Cognito sends the user after login
-    callbackUrl:   'https://daikdlt9atbtt.cloudfront.net/callback.html',
+    callbackUrl:      'https://daikdlt9atbtt.cloudfront.net/callback.html',
 
     // Where Cognito sends the user after logout
-    logoutUrl:     'https://daikdlt9atbtt.cloudfront.net/',
+    logoutUrl:        'https://daikdlt9atbtt.cloudfront.net/',
 
-    // OAuth scopes to request
-    scopes:        'email+openid+profile',
+    // OAuth scopes
+    scopes:           'email+openid+profile',
+
+    // S3 buckets
+    inputBucket:      'voicecraft-text-input',
+    outputBucket:     'voicecraft-audio-output',
 };
